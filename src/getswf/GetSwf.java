e6a16d39e809a2d1f704a969cb41dac909e4e7d8
Andr <Andr@DESKTOP-QOJK334>
2015-08-01 19:34:04 +0300

    aded issuu

--- src/getswf/GetSwf.java
+++ src/getswf/GetSwf.java
@@ -26,92 +26,102 @@ import javax.swing.JTextField;
  * @author Andr
  */
 public class GetSwf {
-public final  static Logger log = Logger.getLogger("this_Class");
+
+    public final static Logger log = Logger.getLogger("this_Class");
+
     /**
      * @param args the command line arguments
-     *  http://p2.calameo.com/140829102709-db0b671491bce0b62f8642f7beab1ae4/p1.swf
+     * http://p2.calameo.com/140829102709-db0b671491bce0b62f8642f7beab1ae4/p1.swf
+     * http://page.issuu.com/130114195401-08397d4a891246c09a78233fd1304cc1/swf/page_9.swf
      */
-    
-    
+
     public static void main(String[] args) {
-	
-	  String url = null;
-	
-	if(args.length==0){
-	     JTextField tf = new JTextField();
-	     JOptionPane.showMessageDialog(null,tf,"Введите URL",1);
-	     url = tf.getText();
-	}else {
+
+	String url = null;
+
+	if (args.length == 0) {
+	    JTextField tf = new JTextField();
+	    JOptionPane.showMessageDialog(null, tf, "Введите URL", 1);
+	    url = tf.getText();
+	} else {
 	    url = args[0];
 	}
 	log.info(url);
-	if(!url.isEmpty())
+	if (!url.isEmpty()) {
 	    counter(geturl(url));
+	}
     }
-    
-    private static  void counter(String url ){
-        int i = 0;
-	while(true){
-	    String fileName  = "p" + (++i) + ".swf";   
+
+    private static void counter(String url) {
+	String sufix = ".swf";
+	String prefix = null;
+	if(url.contains("calameo")){
+	  prefix = "p";
+	}else if(url.contains("issuu")){
+	   prefix = "page_";
+	}
+	
+	
+	int i = 0;
+	while (true) {
+	    String fileName = prefix + (++i) + sufix;
 	    GetSwf gs = new GetSwf();
-	    
+
 	    try {
-		URL u = new URL(url+fileName);
+		URL u = new URL(url + fileName);
 		gs.fileWrite(new File("./files/" + fileName), gs.getData(u));
+		
 		log.log(Level.INFO, "operation with url : {0}  - is successful", u.toString());
 	    } catch (MalformedURLException ex) {
 		Logger.getLogger(GetSwf.class.getName()).log(Level.SEVERE, null, ex);
 	    } catch (Exception ex) {
-		JOptionPane.showMessageDialog(null, "Скачано "+ (i-1) +" файлa/(ов)","Finish",1);
+		JOptionPane.showMessageDialog(null, "Скачано " + (i - 1) + " файлa/(ов)", "Finish", 1);
 		break;
 	    }
-	
+
 	}
 	createEX();
 	log.info("finish");
-    
+
     }
-    
-    private static  void createEX(){
-       File f = new File("./files/EX/");
-       if(!f.exists()){
+
+    private static void createEX() {
+	File f = new File("./files/EX/");
+	if (!f.exists()) {
 	    f.mkdirs();
-       }
-    
-    
-    
+	}
+
     }
-    
-    private static  String geturl(String url){
-	 String s[] = url.split("/");
-	 url = url.replace(s[s.length-1], "");
-	 System.out.println(url);
-         return url;
+
+    private static String geturl(String url) {
+	String s[] = url.split("/");
+	url = url.replace(s[s.length - 1], "");
+	System.out.println(url);
+	return url;
     }
-    
-    
-    private ReadableByteChannel getData(URL url) throws Exception{
-	 ReadableByteChannel channel = null;
+
+    private ReadableByteChannel getData(URL url) throws Exception {
+	ReadableByteChannel channel = null;
 	try {
 	    HttpURLConnection conn;
-	   
-	    conn = (HttpURLConnection)url.openConnection();
+
+	    conn = (HttpURLConnection) url.openConnection();
 	    conn.setRequestMethod("GET");
-	    
-	    if(conn.getResponseCode()==200){
-	        channel = Channels.newChannel(conn.getInputStream());
-	    }else{
-	         throw new Exception();
+
+	    if (conn.getResponseCode() == 200) {
+		channel = Channels.newChannel(conn.getInputStream());
+	    } else {
+		throw new Exception();
 	    }
-	    
+
 	} catch (IOException ex) {
 	    Logger.getLogger(GetSwf.class.getName()).log(Level.SEVERE, null, ex);
 	}
 	return channel;
     }
-    
-    private void fileWrite(File f, ReadableByteChannel rbc){
-    
+
+    private void fileWrite(File f, ReadableByteChannel rbc) {
+
 	if (!f.exists()) {
 	    File folders = f.getParentFile();
 	    folders.mkdirs();
@@ -140,16 +150,6 @@ public final  static Logger log = Logger.getLogger("this_Class");
 	    }
 	}
 
-	
-	
-    
     }
 
-  
-    
-    
-    
-    
-    
-    
 }
