87722ae21154b1a9adfd1d0a0980b87cb269c59d
Andr <andxbes@gmail.com>
2015-08-09 15:38:53 +0300

    fixed the mini test =)

--- src/test/java/getswf/GetSwfTest.java
+++ src/test/java/getswf/GetSwfTest.java
@@ -4,13 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class GetSwfTest {
 
 	@Test
 	public void test() {
 		String s = "http://p2.calameo.com/140829102709-db0b671491bce0b62f8642f7beab1ae4/p1.swf";
 	
-		System.out.println(GetSwf.getGeneratedFolderName(s));
+		Assert.assertTrue(!(GetSwf.getGeneratedFolderName(s).contains(".")));
+		Assert.assertTrue(!(GetSwf.getGeneratedFolderName(s).contains("/")));
+	
 	}
 
 }
